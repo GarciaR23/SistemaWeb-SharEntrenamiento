d0065a84a9967c61e63f4d24bd41ca5a3e7f8039
Gerardo Ramos <jramos5191@gmail.com>
2026-06-02 18:49:14 -0500

    Implementacion de Rutas para no entrar en conflicto con el security

--- backend/src/main/java/edu/utp/backend/core/security/SecurityConfig.java
+++ backend/src/main/java/edu/utp/backend/core/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package edu.utp.backend.core.security;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,6 +31,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/public/status").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/cloudinary/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/instructores", "/api/tutores", "/api/pacientes",
+                         "/api/documentos").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
